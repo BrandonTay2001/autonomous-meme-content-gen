c3a3f9cf567acabaf5a4e5efbe34b162f7581e77
BrandonTay2001 <brandontay2001@gmail.com>
2024-12-07 19:15:36 +0800

    refactor clients

---
+++ clients.py
@@ -0,0 +1,29 @@
+from dotenv import load_dotenv, find_dotenv
+import os
+from mistralai import Mistral
+from pyht import Client as PlayHtClient
+from tweepy import Client as TweepyClient, OAuth1UserHandler, API as TweepyV1API
+
+load_dotenv(find_dotenv())
+
+tweet_client = TweepyClient(bearer_token=os.getenv("BEARER_TOKEN_TWITTER"), 
+                            consumer_key=os.getenv("API_KEY_TWITTER"), 
+                            consumer_secret=os.getenv("API_SECRET_TWITTER"), 
+                            access_token=os.getenv("ACCESS_TOKEN_TWITTER"), 
+                            access_token_secret=os.getenv("ACCESS_TOKEN_SECRET_TWITTER"))
+
+auth = OAuth1UserHandler(
+    consumer_key=os.getenv("API_KEY_TWITTER"),
+    consumer_secret=os.getenv("API_SECRET_TWITTER"),
+    access_token=os.getenv("ACCESS_TOKEN"),
+    access_token_secret=os.getenv("ACCESS_TOKEN_SECRET_TWITTER")
+)
+
+media_upload_client = TweepyV1API(auth)
+
+llm_client = Mistral(api_key=os.environ.get('MISTRAL_API_KEY'))
+
+play_ht_client = PlayHtClient(
+    user_id=os.getenv("PLAY_HT_USER_ID"),
+    api_key=os.getenv("PLAY_HT_API_KEY")
+)
--- main.py
+++ main.py
@@ -9,39 +9,15 @@ from util.ContentIdeaGenerator import ContentIdeaGenerator
 from pipelines.AnimatedImagePipeline import AnimatedImagePipeline
 from pipelines.TextTweetPipeline import TextTweetPipeline
 from pipelines.ImageTweetPipeline import ImageTweetPipeline
-import json
+from clients import llm_client, play_ht_client, tweet_client, media_upload_client
 import os
-from mistralai import Mistral
-from pyht import Client as PlayHtClient
-from tweepy import Client as TweepyClient, OAuth1UserHandler, API as TweepyV1API
 import time
 
 load_dotenv(find_dotenv())
 pipelines = ["text", "image", "animateImage"]
 
-tweet_client = TweepyClient(bearer_token=os.getenv("BEARER_TOKEN_TWITTER"), 
-                            consumer_key=os.getenv("API_KEY_TWITTER"), 
-                            consumer_secret=os.getenv("API_SECRET_TWITTER"), 
-                            access_token=os.getenv("ACCESS_TOKEN_TWITTER"), 
-                            access_token_secret=os.getenv("ACCESS_TOKEN_SECRET_TWITTER"))
-
-auth = OAuth1UserHandler(
-    consumer_key=os.getenv("API_KEY_TWITTER"),
-    consumer_secret=os.getenv("API_SECRET_TWITTER"),
-    access_token=os.getenv("ACCESS_TOKEN"),
-    access_token_secret=os.getenv("ACCESS_TOKEN_SECRET_TWITTER")
-)
-
-media_upload_client = TweepyV1API(auth)
-
-llm_client = Mistral(api_key=os.environ.get('MISTRAL_API_KEY'))
 with open('config/character-bio.txt', 'r', encoding='utf-8') as f:
     character_bio = f.read()
-
-play_ht_client = PlayHtClient(
-    user_id=os.getenv("PLAY_HT_USER_ID"),
-    api_key=os.getenv("PLAY_HT_API_KEY")
-)
 
 text_gen_util = TextGenUtil(llm_client, character_bio)
 image_gen_util = ImageGenUtil(os.getenv("REPLICATE_LORA_MODEL"))
